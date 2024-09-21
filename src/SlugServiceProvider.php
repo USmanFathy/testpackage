2c964f557af1a3286d6c734d35b6b14f47354cf8
USmanFathy <usmanahmedfathy@gmail.com>
2024-09-21 16:10:33 +0300

    update

--- src/SlugServiceProvider.php
+++ src/SlugServiceProvider.php
@@ -5,7 +5,7 @@ use Illuminate\Support\ServiceProvider;
 
 class SlugServiceProvider extends ServiceProvider{
     public function boot(){
-        $this->loadRoutesFrom(__DIR__.'../routes/web.php');
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
     }
 
     public function register()
